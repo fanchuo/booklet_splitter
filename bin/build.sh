ded78a0f975080c514e405ea46c71b61e40f09cf
Francois Secherre <secherre.francois@gmail.com>
2021-10-29 23:59:00 +0200

    Better pipelines

--- bin/build.sh
+++ bin/build.sh
@@ -8,31 +8,40 @@ if [ ! -d venv ] ; then
 fi
 
 source venv/bin/activate
-if ! pip show --quiet booklet_splitter ; then
+if ! pip show --quiet wheel ; then
   pip install -r requirements.txt
-  pip install -e .[tests]
 fi
+ensure_installed() {
+  if ! pip show --quiet booklet_splitter ; then
+    pip install -e .[tests]
+  fi
+}
 
 while [ $# -gt 0 ] ; do
   case "$1" in
     test)
+      ensure_installed
       python setup.py test
       ;;
     package)
       python setup.py bdist_wheel
       ;;
     cover)
+      ensure_installed
       coverage run setup.py test
       coverage report
       coverage html
       ;;
     flake8)
+      ensure_installed
       flake8 src/ tests/ scripts/booklets
       ;;
     black-check)
+      ensure_installed
       black --check src/ tests/ scripts/booklets
       ;;
     black)
+      ensure_installed
       black src/ tests/ scripts/booklets
       ;;
   esac
